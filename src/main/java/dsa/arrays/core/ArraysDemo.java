551b67dfdbc3d2a7f3e08ffad382016a75413051
harshPareek-git <harshpareek.git@gmail.com>
2025-09-02 11:38:47 +0530

    first commit

--- src/main/java/dsa/arrays/core/ArraysDemo.java
+++ src/main/java/dsa/arrays/core/ArraysDemo.java
@@ -1,7 +1,33 @@
 package dsa.arrays.core;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArraysDemo {
     public static void main(String[] args) {
-        System.out.println("HEllO");
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[3][3];
+            for (int i = 0; i < arr.length ; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+       /* for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length ; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }*/
+
+        /*for (int i = 0; i < arr.length ; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }*/
+
+        for(int[] i: arr){
+            System.out.println(Arrays.toString(i));
+        }
+        
+
     }
 }
